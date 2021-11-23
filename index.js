ef8c74fb0b0541907966afe59b1d2547f43c8e65
Jennifer Hong <hongjenn24@gmail.com>
2021-11-22 18:27:40 -0800

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
   // Write your code here!
+
+  if (feet <= 400) {
+    return "This one is on me!";
+    }
+    else if (feet > 2000 && feet < 2500) {
+      return "I will gladly take your thirty bucks.";
+    }
+    else if (feet > 2500) {
+      return "No can do.";
+    }
+  
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(input){
   // Write your code here!
+
+  return input === 'NYC' ? 'Ok, sounds good.' : 'No go.';
+
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip) {
   // Write your code here!
+
+switch (tip) {
+  case 'generous':
+    return 'Thank you so much.';
+  case 'not as generous':
+    return 'Thank you.';
+  default:
+    return 'Bye.';
+
+}
+
+
 }
