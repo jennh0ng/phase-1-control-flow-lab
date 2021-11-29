b08559cddaead3ab0cd53c349a79a11c7f8c4c3c
Jennifer Hong <hongjenn24@gmail.com>
2021-11-29 02:18:48 -0800

    commit tree 14038c101a1b8337a1c10e09b0be386fbacac411

--- index.js
+++ index.js
@@ -1,37 +1,64 @@
-function scuberGreetingForFeet(feet){
-  // Write your code here!
+// function scuberGreetingForFeet(feet){
+//   // Write your code here!
 
+//   if (feet <= 400) {
+//     return "This one is on me!";
+//     }
+//     else if (feet > 2000 && feet < 2500) {
+//       return "I will gladly take your thirty bucks.";
+//     }
+//     else if (feet > 2500) {
+//       return "No can do.";
+//     }
+// }
+
+// function ternaryCheckCity(input){
+//   // Write your code here!
+
+//   return input === 'NYC' ? 'Ok, sounds good.' : 'No go.';
+// }
+
+// function switchOnCharmFromTip(tip) {
+//   // Write your code here!
+
+// switch (tip) {
+//   case 'generous':
+//     return 'Thank you so much.';
+//   case 'not as generous':
+//     return 'Thank you.';
+//   default:
+//     return 'Bye.';
+//   }
+// }
+
+
+// write your function using if and if else statements
+
+function scuberGreetingForFeet(feet) {
   if (feet <= 400) {
-    return "This one is on me!";
-    }
-    else if (feet > 2000 && feet < 2500) {
-      return "I will gladly take your thirty bucks.";
-    }
-    else if (feet > 2500) {
-      return "No can do.";
-    }
-  
+    return 'This one is on me!';
+  }
+  else if (feet > 2000 && feet < 2499) {
+    return 'I will gladly take your thirty bucks.';
+  }
+  else if (feet > 2500 ) {
+    return 'No can do.';
+  }
 }
 
-function ternaryCheckCity(input){
-  // Write your code here!
+// write your function using ternary operator
 
-  return input === 'NYC' ? 'Ok, sounds good.' : 'No go.';
 
+function ternaryCheckCity(city) {
+  return (city === 'NYC' ? 'Ok, sounds good.' : 'No go.')
 }
 
 function switchOnCharmFromTip(tip) {
-  // Write your code here!
-
-switch (tip) {
-  case 'generous':
-    return 'Thank you so much.';
-  case 'not as generous':
-    return 'Thank you.';
-  default:
-    return 'Bye.';
-
-}
-
-
+  switch (tip) {
+    case 'generous': return 'Thank you so much.';
+    break;
+    case 'not as generous': return 'Thank you.';
+    break;
+    default: return 'Bye.';
+  } 
 }
